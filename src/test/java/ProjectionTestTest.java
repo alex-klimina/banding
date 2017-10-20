b278913b2a09d3d0a6b1a33c5747c56e9026ae25
alex-klimina <alex.klimina@yandex.ru>
2017-10-20 20:20:55 +0300

    delete unused test

--- src/test/java/ProjectionTestTest.java
+++ src/test/java/ProjectionTestTest.java
@@ -20,22 +20,4 @@ public class ProjectionTestTest {
         assertThat(ProjectionTest.countProjection(queryIntervals, referenceIntervals), is(5));
     }
 
-    @Test
-    public void printMiddles() throws IOException {
-        String reference = "/Users/alkli/Documents/Yandex.Disk/BioInstitute/banding/banding/src/test/resources/ref.txt";
-        String query = "/Users/alkli/Documents/Yandex.Disk/BioInstitute/banding/banding/src/test/resources/query.txt";
-
-//        List<Interval> referenceIntervals = new IntervalReader(reference).read();
-        Queue<Interval> queryIntervals = new IntervalReader(query).read();
-
-        queryIntervals.stream()
-//                .map(ProjectionTest::middleOfInterval)
-                .forEach(System.out::println);
-
-        queryIntervals.stream()
-                .map(ProjectionTest::middleOfInterval)
-                .forEach(System.out::println);
-
-    }
-
 }
