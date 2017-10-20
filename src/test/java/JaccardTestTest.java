d0c747a3923fdbd3c8291dc52adc8e84ff893f8d
alex-klimina <alex.klimina@yandex.ru>
2017-10-20 20:22:04 +0300

    use Queue for tracks add test shouldComputeIntersectionOfIntervals() add shouldReturnWhetherIntervalIntersectedOrNot()

--- src/test/java/JaccardTestTest.java
+++ src/test/java/JaccardTestTest.java
@@ -1,21 +1,62 @@
 import org.junit.Test;
 
 import java.io.IOException;
+import java.util.ArrayDeque;
 import java.util.List;
+import java.util.Queue;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.*;
 
 public class JaccardTestTest {
 
+    @Test
+    public void shouldComputeIntersectionOfIntervals() {
+        Queue<Interval> reference = new ArrayDeque<>();
+        reference.add(new Interval(5, 55));
+        reference.add(new Interval(204, 255));
+
+        Queue<Interval> query = new ArrayDeque<>();
+        query.add(new Interval(5, 20));
+        query.add(new Interval(27, 42));
+        query.add(new Interval(47, 62));
+        query.add(new Interval(197, 212));
+        query.add(new Interval(219, 234));
+        query.add(new Interval(242, 257));
+
+        assertThat(JaccardTest.getIntersectionValue(reference, query), is(15+(42-27)+(55-47) + (212-204)+(234-219)+(255-242)));
+    }
+
+
+    @Test
+    public void shouldComputeUnionOfIntervals() {
+        Queue<Interval> reference = new ArrayDeque<>();
+        reference.add(new Interval(5, 55));
+        reference.add(new Interval(204, 255));
+
+        Queue<Interval> query = new ArrayDeque<>();
+        query.add(new Interval(5, 20));
+        query.add(new Interval(27, 42));
+        query.add(new Interval(47, 62));
+        query.add(new Interval(197, 212));
+        query.add(new Interval(219, 234));
+        query.add(new Interval(242, 257));
+
+        Queue<Interval> expectedTrack = new ArrayDeque<>();
+        expectedTrack.add(new Interval(5, 62));
+        expectedTrack.add(new Interval(197, 257));
+
+        assertArrayEquals(JaccardTest.tracksUnion(reference, query).toArray(), expectedTrack.toArray());
+
+    }
 
     @Test
     public void shouldJaccardTestReturn0643() throws IOException {
         String reference = "/Users/alkli/Documents/Yandex.Disk/BioInstitute/banding/banding/src/test/resources/ref.txt";
         String query = "/Users/alkli/Documents/Yandex.Disk/BioInstitute/banding/banding/src/test/resources/query.txt";
 
-        List<Interval> referenceIntervals = new IntervalReader(reference).read();
-        List<Interval> queryIntervals = new IntervalReader(query).read();
+        Queue<Interval> referenceIntervals = new IntervalReader(reference).read();
+        Queue<Interval> queryIntervals = new IntervalReader(query).read();
 
         assertThat(JaccardTest.compute(queryIntervals, referenceIntervals), is(0.643));
     }
@@ -26,13 +67,12 @@ public class JaccardTestTest {
         String reference = "/Users/alkli/Documents/Yandex.Disk/BioInstitute/banding/banding/src/test/resources/ref.txt";
         String query = "/Users/alkli/Documents/Yandex.Disk/BioInstitute/banding/banding/src/test/resources/query.txt";
 
-        List<Interval> referenceIntervals = new IntervalReader(reference).read();
-        List<Interval> queryIntervals = new IntervalReader(query).read();
+        Queue<Interval> referenceIntervals = new IntervalReader(reference).read();
+        Queue<Interval> queryIntervals = new IntervalReader(query).read();
 
         System.out.println("==== union ");
 
-        queryIntervals.stream()
-                .map(interval -> JaccardTest.intervalAndTrackUnion(interval, referenceIntervals))
+        JaccardTest.tracksUnion(( queryIntervals),  referenceIntervals).stream()
                 .forEach(System.out::println);
 
         System.out.println("==== intersection ");
@@ -41,6 +81,18 @@ public class JaccardTestTest {
                 .map(interval -> JaccardTest.intervalAndTrackIntersection(interval, referenceIntervals))
                 .forEach(System.out::println);
 
+
+    }
+
+    @Test
+    public void shouldReturnWhetherIntervalIntersectedOrNot() {
+        Interval interval1 = new Interval("", 0, 5);
+        Interval interval2 = new Interval("", 6, 10);
+        assertThat(JaccardTest.areIntervalIntersected(interval1, interval2), is(false));
+
+        interval1 = new Interval("", 0, 8);
+        interval2 = new Interval("", 5, 10);
+        assertThat(JaccardTest.areIntervalIntersected(interval1, interval2), is(true));
 
     }
 }
