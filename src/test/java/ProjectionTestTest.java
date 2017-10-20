217fe5dc7616feca6559dcff14c66d4365480d3f
alex-klimina <alex.klimina@yandex.ru>
2017-10-20 20:20:21 +0300

    use Queue for tracks

--- src/test/java/ProjectionTestTest.java
+++ src/test/java/ProjectionTestTest.java
@@ -2,6 +2,7 @@ import org.junit.Test;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.Queue;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.*;
@@ -13,8 +14,8 @@ public class ProjectionTestTest {
         String reference = "/Users/alkli/Documents/Yandex.Disk/BioInstitute/banding/banding/src/test/resources/ref.txt";
         String query = "/Users/alkli/Documents/Yandex.Disk/BioInstitute/banding/banding/src/test/resources/query.txt";
 
-        List<Interval> referenceIntervals = new IntervalReader(reference).read();
-        List<Interval> queryIntervals = new IntervalReader(query).read();
+        Queue<Interval> referenceIntervals = new IntervalReader(reference).read();
+        Queue<Interval> queryIntervals = new IntervalReader(query).read();
 
         assertThat(ProjectionTest.countProjection(queryIntervals, referenceIntervals), is(5));
     }
@@ -25,7 +26,7 @@ public class ProjectionTestTest {
         String query = "/Users/alkli/Documents/Yandex.Disk/BioInstitute/banding/banding/src/test/resources/query.txt";
 
 //        List<Interval> referenceIntervals = new IntervalReader(reference).read();
-        List<Interval> queryIntervals = new IntervalReader(query).read();
+        Queue<Interval> queryIntervals = new IntervalReader(query).read();
 
         queryIntervals.stream()
 //                .map(ProjectionTest::middleOfInterval)
