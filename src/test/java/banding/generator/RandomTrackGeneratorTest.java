3cb68d2e1830af6059549a20f8f6fa1e0b45cd81
alex-klimina <alex.klimina@yandex.ru>
2017-11-18 13:23:03 +0300

    added test for generator by Interval and Track

--- src/test/java/banding/generator/RandomTrackGeneratorTest.java
+++ src/test/java/banding/generator/RandomTrackGeneratorTest.java
@@ -1,9 +1,14 @@
 package banding.generator;
 
 import banding.entity.Interval;
+import banding.entity.Track;
 import org.junit.Test;
 
+import java.util.Collections;
+
 import static banding.generator.RandomTrackGenerator.generateRandomInterval;
+import static org.hamcrest.Matchers.greaterThanOrEqualTo;
+import static org.hamcrest.Matchers.lessThanOrEqualTo;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
@@ -26,6 +31,12 @@ public class RandomTrackGeneratorTest {
     @Test
     public void shouldGenerateIntervalInTrack() {
         Interval originInterval = new Interval(4, 9);
+        Track originTrack = new Track(Collections.singletonList(new Interval(0, 15)));
+        Interval randomInterval = generateRandomInterval(originInterval, originTrack);
+
+        assertThat(randomInterval.getLength(), is(originInterval.getLength()));
+        assertThat(randomInterval.getStartIndex(), greaterThanOrEqualTo(originTrack.getTrackStart()));
+        assertThat(randomInterval.getEndIndex(), lessThanOrEqualTo(originTrack.getTrackEnd()));
     }
 
 
