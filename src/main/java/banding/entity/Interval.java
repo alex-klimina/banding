484a5843033506f17e9323ccb5b7757bec1d1629
alex-klimina <alex.klimina@yandex.ru>
2017-11-22 23:46:00 +0300

    added Lombok annotations and removed related methods in Interval.java

--- src/main/java/banding/entity/Interval.java
+++ src/main/java/banding/entity/Interval.java
@@ -1,11 +1,28 @@
 package banding.entity;
 
-import java.util.Objects;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 
+@Getter
+@Setter
+@EqualsAndHashCode
+@ToString
+@AllArgsConstructor
 public class Interval {
     private String name;
     private int startIndex;
     private int endIndex;
+
+    public Interval(int startIndex, int endIndex) {
+        this(null, startIndex, endIndex);
+    }
+
+    public Interval(String name) {
+        this.name = name;
+    }
 
     public static int middleOfInterval(Interval interval) {
         return interval.getStartIndex() + (interval.getEndIndex() - interval.getStartIndex())/2;
@@ -13,21 +30,6 @@ public class Interval {
 
     public static boolean isPointInInterval(int point, Interval interval) {
         return ((interval.getStartIndex() <= point) && (point <= interval.getEndIndex()));
-    }
-
-    public Interval(String name, int startIndex, int endIndex) {
-        this.name = name;
-        this.startIndex = startIndex;
-        this.endIndex = endIndex;
-    }
-
-    public Interval(String name) {
-        this.name = name;
-    }
-
-    public Interval(int startIndex, int endIndex) {
-        this.startIndex = startIndex;
-        this.endIndex = endIndex;
     }
 
     public static boolean areIntervalsIntersected(Interval interval1, Interval interval2) {
@@ -59,54 +61,6 @@ public class Interval {
         } else {
             return new Interval(-1,-1);
         }
-    }
-
-    @Override
-    public String toString() {
-        return "banding.Interval{" +
-                "name='" + name + '\'' +
-                ", startIndex=" + startIndex +
-                ", endIndex=" + endIndex +
-                '}';
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Interval interval = (Interval) o;
-        return startIndex == interval.startIndex &&
-                endIndex == interval.endIndex;
-    }
-
-    @Override
-    public int hashCode() {
-
-        return Objects.hash(startIndex, endIndex);
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getStartIndex() {
-        return startIndex;
-    }
-
-    public void setStartIndex(int startIndex) {
-        this.startIndex = startIndex;
-    }
-
-    public int getEndIndex() {
-        return endIndex;
-    }
-
-    public void setEndIndex(int endIndex) {
-        this.endIndex = endIndex;
     }
 
     public int getLength() {
