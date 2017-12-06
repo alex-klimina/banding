f024d0688f10f2e905d63523e93cfdd33a2b6677
alex-klimina <alex.klimina@yandex.ru>
2017-12-06 21:54:13 +0300

    added public static int countProjection(Map<String, Track> referenceMap, Map<String, Track> queryMap) private static long countProjection(Track reference, Track query)

--- src/main/java/banding/metric/ProjectionTest.java
+++ src/main/java/banding/metric/ProjectionTest.java
@@ -2,15 +2,30 @@ package banding.metric;
 
 import banding.IntervalReader;
 import banding.entity.Interval;
+import banding.entity.Track;
 
 import java.io.IOException;
+import java.util.Map;
 import java.util.Queue;
 
 import static banding.entity.Track.isPointInAnyIntervalOf;
 
 public class ProjectionTest {
 
-    public static long countProjection(Queue<Interval> queryIntervals, Queue<Interval> referenceIntervals) {
+    public static int countProjection(Map<String, Track> referenceMap, Map<String, Track> queryMap) {
+        int projectionCount = 0;
+
+        for (Map.Entry<String, Track> entry: referenceMap.entrySet()) {
+            projectionCount += countProjection(entry.getValue(), queryMap.get(entry.getKey()));
+        }
+        return projectionCount;
+    }
+
+    private static long countProjection(Track reference, Track query) {
+        return countProjection(reference.getIntervals(), query.getIntervals());
+    }
+
+    public static long countProjection(Queue<Interval> referenceIntervals, Queue<Interval> queryIntervals) {
         return queryIntervals.stream()
                 .map(Interval::middleOfInterval)
                 .filter(x -> isPointInAnyIntervalOf(x, referenceIntervals))
