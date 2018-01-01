db838f1089dbb741317097bbebea47d01eb2a02e
alex-klimina <alex.klimina@yandex.ru>
2018-01-01 22:08:03 +0700

    [tests] added checkCreatingTrackThroughDequeAndListContainsEqualArrayInterval()

--- src/test/java/banding/entity/TrackTest.java
+++ src/test/java/banding/entity/TrackTest.java
@@ -2,8 +2,14 @@ package banding.entity;
 
 import org.junit.Test;
 
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.List;
+
 import static banding.entity.Track.areIntervalAndTrackIntersect;
 import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
 
 public class TrackTest {
@@ -19,5 +25,26 @@ public class TrackTest {
 
         assertThat(areIntervalAndTrackIntersect(new Interval(16, 25), track), is(false));
 
+    }
+
+    @Test
+    public void checkCreatingTrackThroughDequeAndListContainsEqualArrayInterval() {
+        Interval interval1 = new Interval(0, 4);
+        Interval interval2 = new Interval(6, 12);
+        Interval interval3 = new Interval(14, 40);
+
+        Deque<Interval> intervalDeque = new ArrayDeque<>();
+        intervalDeque.add(interval1);
+        intervalDeque.add(interval2);
+        intervalDeque.add(interval3);
+
+        List<Interval> intervalList = new ArrayList<>();
+        intervalList.add(interval1);
+        intervalList.add(interval2);
+        intervalList.add(interval3);
+
+        Track trackCreatedByDeque = new Track(intervalDeque);
+        Track trackCreatedByList = new Track(intervalList);
+        assertEquals(trackCreatedByDeque.getIntervals().toArray(), trackCreatedByList.getIntervals().toArray());
     }
 }
