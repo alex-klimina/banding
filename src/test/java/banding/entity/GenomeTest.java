45293f1e22165a156ba72553d22594eb4e785a43
alex-klimina <alex.klimina@yandex.ru>
2018-01-02 00:35:50 +0700

    [tests] extracted chr1, chr2; added shouldComputeCoverageOfGenome() and shouldGetChromosomeByName()

--- src/test/java/banding/entity/GenomeTest.java
+++ src/test/java/banding/entity/GenomeTest.java
@@ -13,6 +13,8 @@ public class GenomeTest {
 
     Track track1;
     Track track2;
+    Chromosome chr1;
+    Chromosome chr2;
     List<Chromosome> chromosomes;
     Genome genome;
 
@@ -22,13 +24,13 @@ public class GenomeTest {
         track1.addInterval(0, 3)
                 .addInterval(5, 8)
                 .addInterval(10, 15);
-        Chromosome chr1 = new Chromosome("chr1", track1, 0, 20);
+        chr1 = new Chromosome("chr1", track1, 0, 20);
 
         track2 = new Track();
         track2.addInterval(0, 3)
                 .addInterval(5, 8)
                 .addInterval(10, 15);
-        Chromosome chr2 = new Chromosome("chr2", track2, 0, 30);
+        chr2 = new Chromosome("chr2", track2, 0, 30);
 
         chromosomes = new ArrayList<>();
         chromosomes.add(chr1);
@@ -52,6 +54,16 @@ public class GenomeTest {
     @Test
     public void shouldGetNumberOfIntervalsInGenome() {
         assertThat(genome.getNumberOfIntervals(), is(6L));
+    }
+
+    @Test
+    public void shouldComputeCoverageOfGenome() {
+        assertThat(genome.getCoverage(), is(28L));
+    }
+
+    @Test
+    public void shouldGetChromosomeByName() {
+        assertThat(genome.getChromosome("chr2"), is(chr2));
     }
 
 }
