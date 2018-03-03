49a059ac5c9544c1c79bf68ab46dff958703cdad
Ann Golubeva <mrako142@mail.ru>
2018-03-03 14:40:00 +0300

    Update BioHack.py

--- BioHack.py
+++ BioHack.py
@@ -1,15 +1,15 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
-import matplotlib
+import matplotlib 
 matplotlib.style.use('ggplot')
 
-#url = ссылка на файл или полный путь к файлу на компе
+#url = ссылка на файл или полный путь к файлу на компе, не забыть раскомментить эту штуку
 #читаем csv
 graph =  pd.read_csv(url, names = ['val','date'], index_col=[1], decimal=',',
-                 parse_dates = True, dayfirst = True)
+                 parse_dates = True, dayfirst = True) #отрисовываем график
 
 graph.plot()
-plt.savefig('graph.png', bbox_inches='tight')
-plt.show()
-plt.close()
+plt.savefig('graph.png', bbox_inches='tight') #сохраняем в формате png и удаляем пробельное изображение вокруг картинки из-за 
+plt.show() #важным использовать plt.show после сохранения фигуры, иначе это не сработает
+plt.close()#способ предотвратить появление фигуры 
